79293d9ebe2aa213442a13331efe2364684650d2
0246908001 <2425400440@live.gctu.edu.gh>
2026-05-30 23:54:21 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+ 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Bayuo Martin Ngmen-namaal
+# STUDENT ID: 2425400440
+# Email: 2425400440@LIVE.GCTU.EDU.GH
+# Grader: AUGUSTUS BUCKMAN 
+#
+# On my honor, Bayuo Martin Ngmen-namaal, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
+
+    print("print out my initials\n\n")
+
+    print("...BMN\n\n")
 
 
+    print("...BBBBBBBB........MMMM....MMMM........NNNN....NNNN....****")
+    print("...BB....BB........MMMM....MMMM........NNNNN...NNNN....****")
+    print("...BB....BB........MM.MM..MM.MM........NN.MM...NNNN....****")
+    print("...BBBBBBBB........MM.MM..MM.MM........NN..NN..NNNN....****")
+    print("...BB....BB........MM.MM..MM.MM........NN...NN.NNNN....****")
+    print("...BB....BB........MM..MMMM..MM........NN....NNNNNN....****")
+    print("...BBBBBBBB........MM..MMMM..MM........NN.....NNNNN....****")
+    print("...BB....BB........MM...MM...MM........NN......NNNN....****")
+    print("...BB....BB........MM...MM...MM........NN.......NNN....****")
+    print("...BBBBBBBB........MM...MM...MM........NN........NN....****")
 
 main()
+
